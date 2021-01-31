097d917432c3add2f56b693499ee55afa1f71396
Özenç Çelik <ozenc.celik@ceng.deu.edu.tr>
2021-01-31 16:17:06 +0300

    Starting to create event for the message queue.

---
+++ src/Common/EventBus/RabbitMq/Common/Constants.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RabbitMq.Common
+{
+    public static class Constants
+    {
+        public const string BASKET_CHECKOUT_QUEUE = "BASKET_CHECKOUT_QUEUE";
+    }
+}
---
+++ src/Common/EventBus/RabbitMq/Producer/RabbitMqProducer.cs
@@ -0,0 +1,51 @@
+﻿using Newtonsoft.Json;
+using RabbitMq.Events;
+using RabbitMQ.Client;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RabbitMq.Producer
+{
+    public class RabbitMqProducer
+    {
+        private readonly IRabbitMqConnection _connection;
+
+        public RabbitMqProducer(IRabbitMqConnection connection)
+        {
+            _connection = connection;
+        }
+
+        public void PublishBasketCheckout(string queueName, BasketCheckoutEvent publishModel)
+        {
+            using (var channel = _connection.CreateModel())
+            {
+                // Handle Message
+                // durable : false means the queue has been stored in memory not a physical storage
+                // exclusive: false means we do not want to use this queue with other connections.
+                channel.QueueDeclare(queue: queueName, durable: false,
+                    exclusive: false, autoDelete: false, arguments: null);
+                var message = JsonConvert.SerializeObject(publishModel);
+                var body = Encoding.UTF8.GetBytes(message);
+
+                // Set Properties
+                IBasicProperties properties = channel.CreateBasicProperties();
+                properties.Persistent = true;
+                properties.DeliveryMode = 2;
+
+                // Send Event To Queue
+                channel.ConfirmSelect();
+                channel.BasicPublish(exchange: "", routingKey: queueName,
+                    mandatory: true, basicProperties: properties, body: body);
+                channel.WaitForConfirmsOrDie();
+
+                // Send Acknowledgement
+                channel.BasicAcks += (sender, eventArgs) =>
+                {
+                    Console.WriteLine("Send RabitMq");
+                };
+                channel.ConfirmSelect();
+            }
+        }
+    }
+}
